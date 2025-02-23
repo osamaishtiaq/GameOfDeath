3aa6f15dc5cfd6abb0013399a7fb10ebc932f6f5
osamaishtiaq <osama.ishtiaq@external.mercell.com>
2025-02-23 16:41:13 +0100

    sample startup

---
+++ GameOfDeath.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GameOfDeath;
+
+var player1 = new Player
+{
+    CardsCollection = new List<Card>(),
+    CardsInDeck = new List<Card>(),
+    CardsOnField = new List<Card>(),
+    CurrentHealth = 5,
+    Name = "Osama"
+};
+
+Console.WriteLine(player1.Name);
